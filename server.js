c283a465a67a2d3263c2812d0c48fab01dc34fca
Juhana Kuparinen <jmkupa@utu.fi>
2020-02-24 18:21:33 +0200

    Testi5

--- server.js
+++ server.js
@@ -7,25 +7,26 @@ const app = express();
 const port = process.env.PORT || 5000;
 //production mode
 if(process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static(__dirname));
-    app.use(express.static(path.join(__dirname, 'public')));
-    //
-    app.get('/api/packages', (req, res) => {
-        //Just put the location of your file as an argument in the parser
-        const packages = parser("./status.real");
-        console.log(packages);
-        res.json(packages);
-    });
-    //
-    app.get('/*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    app.use(express.static('client/build'));
+    //Render index.html
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+// Set static folder
+app.use(express.static(__dirname));
+app.use(express.static('client/public'));
 //build mode
 app.get('/api/packages', (req, res) => {
     //Just put the location of your file as an argument in the parser
     const packages = parser("./status.real");
     console.log(packages);
     res.json(packages);
+});
+//
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "client" , 'public', 'index.html'));
 });
 app.listen(port, () => console.log(`Server started on port ${port}`));
