f6f80ff7ffac551455a7c2d85acd0dfe8766f79f
Juhana Kuparinen <jmkupa@utu.fi>
2020-02-24 16:12:24 +0200

    Muokattu server tiedosto.

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const parser = require("./convertStatusToJSON");
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+//production mode
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => {
+        res.sendfile(path.join(__dirname = 'client/build/index.html')); 
+    });
+}
+//build mode
 app.get('/api/packages', (req, res) => {
     //Just put the location of your file as an argument in the parser
     const packages = parser("./status.real");
     console.log(packages);
     res.json(packages);
 });
-
-const port = 5000;
-
 app.listen(port, () => console.log(`Server started on port ${port}`));
