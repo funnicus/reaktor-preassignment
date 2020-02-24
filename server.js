18d0ef14c74aec938989d970e7058b39a197e4e6
Juhana Kuparinen <jmkupa@utu.fi>
2020-02-24 17:24:00 +0200

    Testi 2

--- server.js
+++ server.js
@@ -6,11 +6,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 //production mode
 if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname));
+    app.use(express.static(path.join(__dirname, 'build')));
+    //
     app.get('/api/packages', (req, res) => {
         //Just put the location of your file as an argument in the parser
         const packages = parser("./status.real");
         console.log(packages);
         res.json(packages);
+    });
+    //
+    app.get('/*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 }
 //build mode
