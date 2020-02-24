63dabe3c62d022e03e5c089bde451bb0a39b1c51
Juhana Kuparinen <jmkupa@utu.fi>
2020-02-24 18:28:00 +0200

    Testi666

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.get('/api/packages', (req, res) => {
+    //Just put the location of your file as an argument in the parser
+    const packages = parser("./status.real");
+    console.log(packages);
+    res.json(packages);
+});
+
 //production mode
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
@@ -18,14 +26,6 @@ if(process.env.NODE_ENV === 'production') {
 // Set static folder
 app.use(express.static(__dirname));
 app.use(express.static('client/public'));
-//build mode
-app.get('/api/packages', (req, res) => {
-    //Just put the location of your file as an argument in the parser
-    const packages = parser("./status.real");
-    console.log(packages);
-    res.json(packages);
-});
-//
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "client" , 'public', 'index.html'));
 });
